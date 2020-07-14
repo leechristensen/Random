a7d66240f88d90b900d7570aba52df6195631fcc
Lee Christensen <leechristensen@users.noreply.github.com>
2020-07-14 11:41:19 -0700

    Create Start-EtwTrace.ps1

---
+++ PowerShellScripts/Start-EtwTrace.ps1
@@ -0,0 +1,74 @@
+# Based on the code found in https://posts.specterops.io/data-source-analysis-and-dynamic-windows-re-using-wpp-and-tracelogging-e465f8b653f7
+function Start-EtwTrace {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position=0, Mandatory=$true)]
+        [Guid]
+        $ProviderGuid,
+
+        [Parameter(Position=1, Mandatory=$true)]
+        [string]
+        $OutputFile,
+
+        [Parameter(Position=2, Mandatory=$true)]
+        [string]
+        $ProcessPath,
+
+        [Parameter(Position=3, Mandatory=$false)]
+        [scriptblock]
+        $ArgumentList,
+
+
+        [Parameter(Mandatory=$false)]
+        [byte]
+        $Level = 0xFF,
+
+        [Parameter(Mandatory=$false)]
+        [UInt32]
+        $Property = 0x40,
+
+
+        [Parameter(Mandatory=$false)]
+        [switch]
+        $Force
+    )
+
+    if((Test-Path $OutputFile -ErrorAction SilentlyContinue)) {
+        Remove-Item -Force $OutputFile -ErrorAction Stop
+    }
+
+    if(!(Test-Path $ProcessPath -ErrorAction SilentlyContinue)) {
+        throw "Could not find the executable $($ProcessPath)"
+    }
+
+    $EtwProviderGuid = $ProviderGuid
+    $SessionName = 'tempLoggingProvider'
+
+
+    $Session = New-EtwTraceSession -Name $SessionName -LogFileMode 0x08000100 -FlushTimer 1 -ErrorAction Stop
+    $TraceProvider = Add-EtwTraceProvider -SessionName $Session.Name -Guid $EtwProviderGuid -MatchAnyKeyword 0xFFFFFFFFFFFF -Level $Level -Property $Property
+    
+    
+    $TraceProcess = Start-Process -PassThru tracerpt.exe -ArgumentList "-rt $($SessionName) -y -o $($OutputFile) -of EVTX" -WindowStyle Hidden
+
+    sleep 1
+
+    $ProcArgs = @{ FilePath = $ProcessPath }
+    if($ArgumentList) {
+        $ProcArgs['ArgumentList'] = $ArgumentList
+    }
+    
+    $TargetProc = Start-Process @ProcArgs -PassThru -Wait
+    Write-Warning "Analyzed Process ID: $($TargetProc.Id)"
+    
+
+    sleep -Milliseconds 1200  # Wait a bit for the last events to come in
+
+    Write-Warning "Removing the trace session '$($SessionName)'..."
+    $TraceProcess.CloseMainWindow()
+
+    Remove-EtwTraceSession -Name $SessionName
+    Sleep 1
+    
+    Get-WinEvent -Path $OutputFile | sort TimeCreated 
+}
